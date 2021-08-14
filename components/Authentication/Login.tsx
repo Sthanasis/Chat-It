88d42edf1475447405d25a2b3071b6680ac63d06
sthanasis <t.simos@atc.gr>
2021-08-14 13:49:29 +0300

    Add sign in and sign up routes. Add links on toolbar.

--- components/Authentication/Login.tsx
+++ components/Authentication/Login.tsx
@@ -1,10 +1,41 @@
-const Login = (): JSX.Element => {
+import { useState } from 'react';
+import Button from '../UI/Button';
+import Input from '../UI/Input';
+import styles from '../../styles/Form.module.css';
+
+interface Props {
+  toSignUp: () => void;
+}
+
+const Login = ({ toSignUp }: Props): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <form method="POST" action="/api/auth">
-      <input type="text" name="username" value="admin" />
-      <br />
-      <input type="password" name="password" value="admin" />
-      <input type="submit" value="Login" />
+    <form onSubmit={(e) => e.preventDefault()}>
+      <div className={styles.LoginForm}>
+        <Input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="Email"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          label="Password"
+        />
+      </div>
+
+      <div className={styles.FormButtons}>
+        <Button onClick={() => {}} type="submit">
+          Submit
+        </Button>
+        <Button type="transparent" onClick={toSignUp} style={{}}>
+          Not registered? Sign Up.
+        </Button>
+      </div>
     </form>
   );
 };
